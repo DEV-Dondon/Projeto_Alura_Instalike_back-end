25e7d84822817d001273a7b950ed5db23162b8a3
DEV-Dondon <filholuccivan@gmail.com>
2024-11-21 10:28:48 -0300

    oioi

--- server.cjs
+++ server.cjs
@@ -1,42 +1,10 @@
 import express from "express";
-import conectarAoBanco from "./src/config/dbConfig.js";
-
-let conexao;
-const { inicializarConexao } = require('./conexao');
-inicializarConexao();
-
-
-const posts = [
-  { id: 1, descricao: "Uma foto teste", imagem: "https://placekitten.com/300/150" },
-  { id: 2, descricao: "Gato fazendo yoga", imagem: "https://placekitten.com/300/150" },
-  { id: 3, descricao: "Gato fazendo panqueca", imagem: "https://placekitten.com/300/150" },
-];
+import routes from "./src/routes/postsRoutes.js";
 
 const app = express();
+routes(app)
 
+// Inicia o servidor na porta 3000 e exibe uma mensagem no console
 app.listen(3000, () => {
-  console.log("Servidor escutando...");
-});
-
-app.get("/posts", async (req, res) => {
-  try {
-    const resultado = await getTodosPosts();
-    res.status(200).json(resultado);
-  } catch (error) {
-    res.status(500).json({ erro: "Erro ao buscar os posts." });
-  }
-});
-
-function buscarPostPorID(id) {
-  return posts.find((post) => post.id === Number(id));
-}
-
-app.get("/posts/:id", (req, res) => {
-  const post = buscarPostPorID(req.params.id);
-
-  if (!post) {
-    return res.status(404).json({ erro: "Post não encontrado." });
-  }
-
-  res.status(200).json(post);
+    console.log("Servidor escutando...");
 });
--- src/models/postsModel.js
+++ src/models/postsModel.js
@@ -1,20 +1,17 @@
-import 'dotenv/config';
-import { ObjectId } from "mongodb";
 import conectarAoBanco from "../config/dbConfig.js"
-
 const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
+
 export async function getTodosPosts() {
   const db = conexao.db("imersao-instabytes");
   const colecao = db.collection("posts");
-
   return colecao.find().toArray();
 }
+
 export async function criarPost(novoPost) {
   const db = conexao.db("imersao-instabytes");
   const colecao = db.collection("posts");
-  return colecao.insertOne(novoPost);
+  return colecao.insertOne(novoPost)
 }
-
 export async function atualizarPost(id, novoPost) {
   const db = conexao.db("imersao-instabytes");
   const colecao = db.collection("posts");
