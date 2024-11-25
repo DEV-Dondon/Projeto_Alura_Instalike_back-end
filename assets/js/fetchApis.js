c7450da86fe1fec25e5f5da6849fe5b9db887210
DEV-Dondon <filholuccivan@gmail.com>
2024-11-24 21:57:22 -0300

    oi

--- assets/js/fetchApis.js
+++ assets/js/fetchApis.js
@@ -4,7 +4,7 @@ export default async function fetchImages() {
     { imgUrl: "photos/ju.png", alt: "Foto da Ju", descricao: "Amiga" },
     { imgUrl: "photos/veneza.png", alt: "Foto de Veneza", descricao: "Viagem" },
     { imgUrl: "photos/robotica.jpeg", alt: "Robótica", descricao: "Futuro tecnológico" },
-    { imgUrl: "photos/3ef5ceb7-6334-44f8-9cf4-dc140a60821b.jpeg", alt:"Imagem 1", descricao: "Lucivan Filho" },
-    { imgUrl: "photos/Leonardo_Diffusion_Harley_Quinn_semiprofile_wrinkled_face_brig_0.jpg", alt: "Leonardo", descricao: "Leonardo Vampiro" },
+    { imgUrl: "photos/oioi.jpeg", alt:"Imagem 1", descricao: "Lucivan Filho" },
+    { imgUrl: "photos/Leonardo.jpg", alt: "Leonardo", descricao: "Leonardo Vampiro" },
   ];
 }
